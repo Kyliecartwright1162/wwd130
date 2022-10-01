56d9d21e72731cd0ae04e4fe1b8916712a02712b
Kyliecartwright1162 <neb22002@byui.edu>
2022-10-01 14:54:01 -0600

    Added HTML, CSS, & SCRIPTS

---
+++ wwr/scripts/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.hero-slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        fade: true,
+        cssEase: 'linear',
+        speed: 1000,
+        infinite: true
+    });
+});
